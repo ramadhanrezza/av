e82fa825bda2548b7255ebb30c3315f90193a44e
M Rezza Ramadhan <mrezza.ramadhan@gmail.com>
2019-04-12 00:02:14 +0700

    Edit register user

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -4,20 +4,39 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\User;
+Use Exception;
 
 class ApiController extends Controller
 {
+    private $success;
+    private $data;
+    private $code;
+    private $message;
+
     public function register(Request $request) {
-        $user = new User();
-        $user->name = $request->name;
-        $user->email = $request->email;
-        $user->password = bcrypt($request->password);
-        $user->save();
+        try {
+            $user = new User();
+            $user->name = $request->name;
+            $user->email = $request->email;
+            $user->password = bcrypt($request->password);
+            $user->save();
+
+            $this->code = 200;
+            $this->success = true;
+            $this->data = $user;
+            $this->message = 'Record completed.';
+        } catch (Exception $e) {
+            $this->code = 400;
+            $this->success = false;
+            $this->data = [];
+            $this->message = $e->getMessage();
+        }
 
         return response()->json([
-            'success' => true,
-            'data' => $user
-        ], 200);
+            'success'   => $this->success,
+            'message'   => $this->message,
+            'data'      => $this->data
+        ], $this->code);
     }
 
 
